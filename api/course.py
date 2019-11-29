9b6bdcd547f2979d990c931955688c2312fedc48
shengxiaomai <734978582@qq.com>
2019-11-29 11:12:00 +0800

    增加配置文件

--- api/course.py
+++ api/course.py
@@ -5,19 +5,19 @@ import time
 import calendar
 
 class courseApi(BaseApi):
-    url="https://radio-api-demo.zlzgy.org.cn/api/course/newapp/course/getHeadlineList?t="+str(int(time.time()))
-
+    HeadlineList="api/course/newapp/course/getHeadlineList?t="+str(int(time.time()))
+    CourseListByRootId="api/course/newapp/course/getCourseListByRootId"
     headers = {"authorization": login.Login.login(), "content - type": "application / json;charset = UTF - 8"}
     @classmethod
     def GetHeadlineLis(self):#课程模块 获取每日速递列表
-        self.json_data = requests.get(self.url,headers = self.headers,verify = False).json()
+        self.json_data = requests.get(self.url+self.HeadlineList,headers = self.headers,verify = False).json()
         self.verbose(self.json_data)
         return self.json_data
 
     @classmethod
     def getCourseListByRootId(self,catalogueId):#课程模块 根据根目录查询所有二级目录下的所有课程
         payload = {'catalogueId': catalogueId, 'key2': str(int(time.time()))}
-        self.json_data =requests.get("https://radio-api-demo.zlzgy.org.cn/api/course/newapp/course/getCourseListByRootId",params=payload,headers = self.headers,verify = False).json()
+        self.json_data =requests.get(self.url+self.CourseListByRootId,params=payload,headers = self.headers,verify = False).json()
         self.verbose(self.json_data)
         return self.json_data
 
